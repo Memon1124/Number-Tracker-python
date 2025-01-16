454fe08970b6e86c422444c02b05cd4bce93dbe1
Memon1124 <j.memon1124@gmail.com>
2025-01-16 01:50:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from key import keys
 
 
 
-number = input("Enter phone number with country code:")
+number = input(+19803344225)
 
 
-check_number = phonenumbers.parse(number)
+check_number = phonenumbers.parse(+19803344563)
 number_location = geocoder.description_for_number(check_number, "en")
 print(number, " : ", number_location)
 
 
 from phonenumbers import carrier
-service_provider = phonenumbers.parse(number)
+service_provider = phonenumbers.parse(+19803344563)
 print("Service provider : ",carrier.name_for_number(service_provider, "en"))
 
 
 from opencage.geocoder import OpenCageGeocode
-geocoder = OpenCageGeocode(keys)
+geocoder = OpenCageGeocode(b3ecd6fb8e664666ab3138b199141b15)
 query = str(number_location)
 results = geocoder.geocode(query)
 
@@ -39,9 +39,3 @@ print("longitude: ",lng)
 import folium
 map_location = folium.Map(location = [lat,lng], zoom_start=8)
 folium.Marker([lat,lng], popup=number_location).add_to(map_location)
-map_location.save("mylocation.html")
-
-import webbrowser
-# here insert the relative path of your mylocation.html file 
-url = " realtive path " 
-webbrowser.open_new(url)
